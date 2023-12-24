a0f8876ad9112b97afbbb9dbf3b872e2b4707685
Apsapeh <timofeykirichenko@gmail.com>
2023-12-25 00:45:59 +0300

    Vector

--- include/extc_vec.h
+++ include/extc_vec.h
@@ -1,4 +1,32 @@
-#include "extc_defdyn.h"
 #include "extc_rint.h"
 
-_export i32 vec_init();
+#define vector_template_def(name, type)\
+    typedef struct vec_##name {\
+        size capacity;\
+        size size;\
+        type* data;\
+    } vec_##name;\
+\
+    vec_##name vec_##name##_init();\
+    void vec_##name##_push_back(vec_##name * v, type o);\
+    void vec_##name##_free(vec_##name * v);
+
+
+
+#define vector_template_impl(name, type)\
+    vec_##name vec_##name##_init() {\
+        vec_##name res;\
+        res.size = 0;\
+        res.capacity = 0;\
+        res.data = malloc(0);\
+        return res;\
+    };\
+    \
+    void vec_##name##_push_back(vec_##name * v, type o) {\
+        v->data = realloc(v->data, (v->size+1) * sizeof(type));\
+        v->data[v->size] = o;\
+    };\
+    \
+    void vec_##name##_free(vec_##name * v) {\
+        free(v->data);\
+    }
--- src/extc_vec.c
+++ src/extc_vec.c
@@ -1,5 +1,23 @@
+/*#include <stdlib.h>
+#include <string.h>
 #include <extc_vec.h>
 
-_export i32 vec_init() {
 
+
+_export vec vec_init(size element_size) {
+    vec res;
+    res.size = 0;
+    res.capacity = 0;
+    res.element_size = element_size;
+    res.data = malloc(0);
+    return res;
 };
+
+_export void vec_push_back(vec* v, void* o) {
+    v->data = realloc(v->data, v->size);
+    //v->data[v->size];
+    v->data[v->size] = malloc(v->element_size);
+    memcpy(v->data[v->size], o, v->element_size);
+    ++v->size;
+    ++v->capacity;
+}*/
