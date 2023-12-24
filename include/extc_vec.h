578e3c65015968e577ded2b259f878990f91cbc2
Apsapeh <timofeykirichenko@gmail.com>
2023-12-24 21:07:04 +0300

    commit

--- include/extc_vec.h
+++ include/extc_vec.h
@@ -1,4 +1,4 @@
 #include "extc_defdyn.h"
 #include "extc_rint.h"
 
-_export void vec_init();
+_export i32 vec_init();
--- src/extc_vec.c
+++ src/extc_vec.c
@@ -1,5 +1,5 @@
 #include <extc_vec.h>
 
-_export void vec_init() {
-    
+_export i32 vec_init() {
+
 };
